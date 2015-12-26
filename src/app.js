df1d0fa5fcded4296c7b3916a59183dca14be3e3
Nicole <nsiegel2@gmail.com>
2015-12-25 21:49:26 -0500

    serving up static folder

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
 
 app.use(routes);
+app.use(express.static(__dirname + '/static'));
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
