4438878e22e8d4a8a7b0ab4db5352593cf572121
Nicole <nsiegel2@gmail.com>
2015-12-25 20:33:35 -0500

    render templates using swig

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var swig = require('swig');
+var routes = require('./routes');
 var app = express();
+
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+app.use(routes);
 
 app.listen(3000, function() {
   console.log('listening on port 3000');
