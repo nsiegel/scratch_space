b29e488fcc04769e2dcbc71bbf0421a1acf2b4e2
Nicole <nsiegel2@gmail.com>
2015-12-27 23:50:48 -0500

    add angular state and bootstrap

--- src/static/app/scratch.js
+++ src/static/app/scratch.js
@@ -1,5 +1,14 @@
-var app = angular.module('scratch', []);
+var app = angular.module('scratch', ['ui.router', 'ui.bootstrap']);
 
-app.controller('main', ['$scope', function($scope) {
+app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider.state("home", {
+    url: '/',
+    template: "<h1>HELLO!</h1>",
+    controller: 'chat'
+  });
+});
+
+app.controller('chat', ['$scope', function($scope) {
   console.log('hello world');
 }]);
