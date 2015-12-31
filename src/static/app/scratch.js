a32d300af556d4df6f0897f78029143a043a3bf2
Nicole <nsiegel2@gmail.com>
2015-12-31 00:27:22 -0500

    removed angular and setup initial canvas

--- src/static/app/scratch.js
+++ src/static/app/scratch.js
@@ -1,14 +1,6 @@
-var app = angular.module('scratch', ['ui.router', 'ui.bootstrap']);
-
-app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
-  $urlRouterProvider.otherwise('/');
-  $stateProvider.state("home", {
-    url: '/',
-    template: "<h1>HELLO!</h1>",
-    controller: 'chat'
-  });
-});
-
-app.controller('chat', ['$scope', function($scope) {
-  console.log('hello world');
-}]);
+var draw = function() {
+  var canvas = getElementsById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+  }
+};
