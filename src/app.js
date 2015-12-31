0e6bb6c6df93d79f98b4202b0a8dc8ffa149b468
Nicole <nsiegel2@gmail.com>
2015-12-31 00:26:39 -0500

    disable swig cache

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ var app = express();
 
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'html');
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
 app.engine('html', swig.renderFile);
 
 app.use(routes);
