54b872ea420a45e234ab82fddaf83b37306fe8c2
Nicole <nsiegel2@gmail.com>
2015-12-31 02:27:35 -0500

    it draws!

--- src/static/app/scratch.js
+++ src/static/app/scratch.js
@@ -1,6 +1,51 @@
-var draw = function() {
-  var canvas = getElementsById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+$(document).ready(function() {
+  var canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  // ctx.fillStyle = "rgba(0,0,0)";
+  var isDrawing = false;
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 1;
+
+  var x = 0;
+  var y = 0;
+
+  var findXY = function(x, y) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: x - canvas.offsetLeft,
+      y: y - canvas.offsetTop
+    }
+  };
+
+  var click = function(e) {
+    isDrawing = true;
+    var pos = findXY(e.pageX, e.pageY);
+    x = pos.x;
+    y = pos.y;
+    // ctx.beginPath();
+    // ctx.moveTo(pos.x, pos.y);
   }
-};
+
+  var draw = function(e) {
+    if (!isDrawing) {
+      console.log('Im not drawing');
+      return;
+    }
+    var pos = findXY(e.pageX, e.pageY);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(pos.x, pos.y);
+    x = pos.x;
+    y = pos.y;
+    console.log("x: " + x + " " + "y: " + y);
+    ctx.stroke();
+  };
+
+  var stopDrawing = function(e) {
+    isDrawing = false;
+  }
+
+  canvas.addEventListener('mousedown', click);
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', stopDrawing);
+});
